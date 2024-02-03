77b4e4fc81796b0036443d7e4f5b6f1e3d492fab
Swarnab Banerjee <116362013+swarnab007@users.noreply.github.com>
2024-02-03 22:01:37 +0530

    api key updated

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -3,7 +3,7 @@ import React from 'react'
 const options = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': 'a24f534d51msh1dde772c9163457p144f03jsne365b8960fe6',
+    'X-RapidAPI-Key': 'a14288d59fmsh2ab2e67fabd1675p1c8b5bjsn92daa6a7c791',
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
   }
 };
